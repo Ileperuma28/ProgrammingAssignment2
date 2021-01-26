89130b8e8f4a8ad3edb9a974929d7e99767f28f5
Ileperuma28 <ileperuma.11.asiri@gmail.com>
2021-01-27 00:04:58 +0530

    assignmet done m3

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
+## I simply set the input a as a matrix
+## and then set the solved value "answer" as a null
+## then I changed every reference to "mean" to "solve"
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(a = matrix()) {
+  
+  answer <- NULL
+  set <- function(b) {
+    a <<- b
+    answer <<- NULL
+  }
+  get <- function() a
+  setanswer <- function(inverse) answer <<- inverse
+  getanswer <- function() answer
+  list(set = set, get = get, setanswer = setanswer, getanswer = getanswer)
+  
 }
+
+
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(a, ...) {
+        ## Return a matrix that is the inverse of 'a'
+  
+  answer <- a$getanswer()
+  if(!is.null(answer)) {
+    message("getting cached result")
+    return(answer)
+  }
+  data <- a$get()
+  answer <- solve(data, ...)
+  a$setanswer(answer)
+  answer
 }
